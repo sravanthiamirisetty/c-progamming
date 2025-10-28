68d8c2c8ff93ec4ac3aec51f25495f91526aa26a
sravanthiamirisetty <sravanthiamirisetty@gmail.com>
2025-10-28 18:25:41 +0530

    Create volume_of_sphere_maya.c

---
+++ volume_of_sphere_maya.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+int cost_price, selling_price;
+scanf("%d %d",&cost_price, &selling_price);
+if(cost_price<selling_price)
+{
+printf("Profit");
+else if(cost_price>selling_price)
+{
+printf("Loss");
+else{
+printf("No Profit and No Loss");
+return 0;
+}
