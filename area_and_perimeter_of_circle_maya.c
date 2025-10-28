07b4a02cdf5a10f0864efe79cf5be0457889095d
sravanthiamirisetty <sravanthiamirisetty@gmail.com>
2025-10-28 18:16:51 +0530

    Create area_and_perimeter_of_circle_maya.c

---
+++ area_and_perimeter_of_circle_maya.c
@@ -0,0 +1,12 @@
+#include<stalo. n>
+int main()
+{
+int radius;
+float area, perimeter;
+scanf("%d",&radius);
+area=(3.14*radius*radius);
+perimeter=(2.0*3.14*radius);
+printf("%.2f", area);
+printf("%.2f", perimeter);
+return 0;
+}
